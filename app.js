9d8d664a9778a1119b854f03bc9325e50be9dc27
alemalagni <alemalagni@gmail.com>
2025-03-27 09:55:05 +0100

    feat: add array post

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//app.use( express.static( 'images' ) );
+const post = [];
+
 app.get( '/', (req, res) => {
     res.type("html")
         .send( `<p>Server del mio blog</p>` )

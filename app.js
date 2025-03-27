7c5b1439395c096994ad02ef2d89b2a8687a9f3e
alemalagni <alemalagni@gmail.com>
2025-03-27 09:35:21 +0100

    add: "/" root

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get( '/', (req, res) => {
+    res.type("html")
+        .send( `<p>Server del mio blog</p>` )
+})
+

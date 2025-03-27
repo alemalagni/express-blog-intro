6e1d1feabfe4ce847a8f31a2ef33f38d2ee934ba
alemalagni <alemalagni@gmail.com>
2025-03-27 09:59:02 +0100

    feat: add express static folder

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//app.use( express.static( 'images' ) );
+app.use( express.static( 'images' ) );
+
 const post = [
     {
     titolo: "titolo 1",
